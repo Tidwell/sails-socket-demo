4452ce5a265b878c013fe277fc1b49b12b67edc4
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-03-11 13:31:55 -0400

    working socket example

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/*---------------------
+	:: User
+	-> model
+---------------------*/
+module.exports = {
+
+	attributes	: {
+
+		// Simple attribute:
+		// name: 'STRING',
+
+		// Or for more flexibility:
+		// phoneNumber: {
+		//	type: 'STRING',
+		//	defaultValue: '555-555-5555'
+		// }
+		
+		username: {
+			type: 'STRING'
+		}
+	}
+
+};
